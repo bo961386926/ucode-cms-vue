cfec984027f1d9c42bbe6e1cd4c7a7b321876043
explore <982361244@qq.com>
2022-01-06 11:18:45 +0800

    整理代码生成容器注入逻辑

--- ucode-cms-admin/src/main/java/xin/ucode/front/service/ISysConfigService.java
+++
@@ -1,6 +0,0 @@
-package xin.ucode.front.service;
-import com.baomidou.mybatisplus.extension.service.IService;
-import xin.ucode.front.domain.SysConfig;
-
-public interface ISysConfigService extends IService<SysConfig>{
-}

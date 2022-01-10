55d77c46f81aae93ab8dc75230e0995590fd902b
explore <982361244@qq.com>
2022-01-10 19:35:24 +0800

    更新版本号：1.2.1

--- ucode-cms-admin/src/main/java/xin/ucode/admin/CmsApplication.java
+++ ucode-cms-admin/src/main/java/xin/ucode/admin/CmsApplication.java
@@ -2,6 +2,7 @@ package xin.ucode.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import xin.altitude.cms.common.util.PathStringUtils;
 
 /**
  * 启动程序

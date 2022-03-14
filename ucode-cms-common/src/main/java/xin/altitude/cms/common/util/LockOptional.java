7830e2c52d8de8acd787ebce81e00f7513bb2013
explore <982361244@qq.com>
2022-03-14 16:20:07 +0800

    更新版本号 增加分布式Redis锁工具类 LockOptional

--- ucode-cms-common/src/main/java/xin/altitude/cms/common/util/LockOptional.java
+++ ucode-cms-common/src/main/java/xin/altitude/cms/common/util/LockOptional.java
@@ -36,85 +36,32 @@ import java.util.function.Supplier;
  * @since 1.4
  */
 public final class LockOptional<T extends RLock> {
-    /**
-     * Common instance for {@code empty()}.
-     */
     private static final LockOptional<?> EMPTY = new LockOptional<>();
 
-    /**
-     * If non-null, the value; if null, indicates no value is present
-     */
     private final T value;
 
-    /**
-     * Constructs an empty instance.
-     *
-     * @implNote Generally only one empty instance, {@link LockOptional#EMPTY},
-     * should exist per VM.
-     */
     private LockOptional() {
         this.value = null;
     }
 
-    /**
-     * Constructs an instance with the value present.
-     *
-     * @param value the non-null value to be present
-     * @throws NullPointerException if value is null
-     */
     private LockOptional(T value) {
         this.value = Objects.requireNonNull(value);
     }
 
-    /**
-     * Returns an empty {@code Optional} instance.  No value is present for this
-     * Optional.
-     *
-     * @param <T> Type of the non-existent value
-     * @return an empty {@code Optional}
-     * @apiNote Though it may be tempting to do so, avoid testing if an object
-     * is empty by comparing with {@code ==} against instances returned by
-     * {@code Option.empty()}. There is no guarantee that it is a singleton.
-     * Instead, use .
-     */
     public static <T extends RLock> LockOptional<T> empty() {
         @SuppressWarnings("unchecked")
         LockOptional<T> t = (LockOptional<T>) EMPTY;
         return t;
     }
 
-    /**
-     * Returns an {@code Optional} with the specified present non-null value.
-     *
-     * @param <T>   the class of the value
-     * @param value the value to be present, which must be non-null
-     * @return an {@code Optional} with the value present
-     * @throws NullPointerException if value is null
-     */
     public static <T extends RLock> LockOptional<T> of(T value) {
         return new LockOptional<>(value);
     }
 
-    /**
-     * Returns an {@code Optional} describing the specified value, if non-null,
-     * otherwise returns an empty {@code Optional}.
-     *
-     * @param <T>   the class of the value
-     * @param value the possibly-null value to describe
-     * @return an {@code Optional} with a present value if the specified value
-     * is non-null, otherwise an empty {@code Optional}
-     */
     public static <T extends RLock> LockOptional<T> ofNullable(T value) {
         return value == null ? empty() : of(value);
     }
 
-    /**
-     * If a value is present in this {@code Optional}, returns the value,
-     * otherwise throws {@code NoSuchElementException}.
-     *
-     * @return the non-null value held by this {@code Optional}
-     * @throws NoSuchElementException if there is no value present
-     */
     public T get() {
         if (value == null) {
             throw new NoSuchElementException("No value present");
@@ -122,60 +69,22 @@ public final class LockOptional<T extends RLock> {
         return value;
     }
 
-    /**
-     * If a value is present, invoke the specified consumer with the value,
-     * otherwise do nothing.
-     *
-     * @param consumer block to be executed if a value is present
-     * @throws NullPointerException if value is present and {@code consumer} is
-     *                              null
-     */
     public void ifLocked(Consumer<? super T> consumer) {
         if (value != null && value.isLocked()) {
             consumer.accept(value);
         }
     }
 
-    /**
-     * Return the value if present, otherwise return {@code other}.
-     *
-     * @param other the value to be returned if there is no value present, may
-     *              be null
-     * @return the value, if present, otherwise {@code other}
-     */
     public T orElse(T other) {
         return value != null ? value : other;
     }
 
-    /**
-     * Return the value if present, otherwise invoke {@code other} and return
-     * the result of that invocation.
-     *
-     * @param other a {@code Supplier} whose result is returned if no value
-     *              is present
-     * @return the value if present otherwise the result of {@code other.get()}
-     * @throws NullPointerException if value is not present and {@code other} is
-     *                              null
-     */
+
     public T orElseGet(Supplier<? extends T> other) {
         return value != null ? value : other.get();
     }
 
-    /**
-     * Return the contained value, if present, otherwise throw an exception
-     * to be created by the provided supplier.
-     *
-     * @param <X>               Type of the exception to be thrown
-     * @param exceptionSupplier The supplier which will return the exception to
-     *                          be thrown
-     * @return the present value
-     * @throws X                    if there is no value present
-     * @throws NullPointerException if no value is present and
-     *                              {@code exceptionSupplier} is null
-     * @apiNote A method reference to the exception constructor with an empty
-     * argument list can be used as the supplier. For example,
-     * {@code IllegalStateException::new}
-     */
+
     public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
         if (value != null) {
             return value;
@@ -184,19 +93,6 @@ public final class LockOptional<T extends RLock> {
         }
     }
 
-    /**
-     * Indicates whether some other object is "equal to" this Optional. The
-     * other object is considered equal if:
-     * <ul>
-     * <li>it is also an {@code Optional} and;
-     * <li>both instances have no value present or;
-     * <li>the present values are "equal to" each other via {@code equals()}.
-     * </ul>
-     *
-     * @param obj an object to be tested for equality
-     * @return {code true} if the other object is "equal to" this object
-     * otherwise {@code false}
-     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -211,27 +107,11 @@ public final class LockOptional<T extends RLock> {
         return Objects.equals(value, other.value);
     }
 
-    /**
-     * Returns the hash code value of the present value, if any, or 0 (zero) if
-     * no value is present.
-     *
-     * @return hash code value of the present value or 0 if no value is present
-     */
     @Override
     public int hashCode() {
         return Objects.hashCode(value);
     }
 
-    /**
-     * Returns a non-empty string representation of this Optional suitable for
-     * debugging. The exact presentation format is unspecified and may vary
-     * between implementations and versions.
-     *
-     * @return the string representation of this instance
-     * @implSpec If a value is present the result must include its string
-     * representation in the result. Empty and present Optionals must be
-     * unambiguously differentiable.
-     */
     @Override
     public String toString() {
         return value != null

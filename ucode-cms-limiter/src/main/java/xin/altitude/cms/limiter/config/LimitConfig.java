ddf7aa5c4382920aa1c1a99a9f12238880b55c40
explore <982361244@qq.com>
2022-03-19 11:24:06 +0800

    更新版本号 增加分布式限流模块 ucode-cms-limiter

---
+++ ucode-cms-limiter/src/main/java/xin/altitude/cms/limiter/config/LimitConfig.java
@@ -0,0 +1,46 @@
+/*
+ *
+ * Copyright (c) 2020-2022, Java知识图谱 (http://www.altitude.xin).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package xin.altitude.cms.limiter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.util.PathMatcher;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import xin.altitude.cms.limiter.interceptor.LimitInterceptor;
+
+/**
+ * 通用配置
+ *
+ * @author ucode
+ */
+@Import({LimitInterceptor.class})
+public class LimitConfig implements WebMvcConfigurer {
+    @Autowired
+    private LimitInterceptor limitInterceptor;
+
+    /**
+     * 自定义拦截规则
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /* 添加全局防重复提交拦截器 */
+        registry.addInterceptor(limitInterceptor).addPathPatterns("/**");
+    }
+}

35f7235421400691cfed65b37f08b4c1d2191b35
explore <982361244@qq.com>
2022-03-21 15:38:15 +0800

    增加分布式日志收集模块 ucode-cms-log

--- ucode-cms-log/src/main/java/xin/altitude/cms/log/domain/OperateLog.java
+++ ucode-cms-log/src/main/java/xin/altitude/cms/log/domain/OperateLog.java
@@ -50,6 +50,11 @@ public class OperateLog {
     private Integer[] businessTypes;
 
     /**
+     * 请求url
+     */
+    private String operUrl;
+
+    /**
      * 请求方法
      */
     private String method;
@@ -75,11 +80,6 @@ public class OperateLog {
     private String deptName;
 
     /**
-     * 请求url
-     */
-    private String operUrl;
-
-    /**
      * 操作地址
      */
     private String operIp;
@@ -89,15 +89,6 @@ public class OperateLog {
      */
     private String operLocation;
 
-    /**
-     * 请求参数
-     */
-    private String operParam;
-
-    /**
-     * 返回参数
-     */
-    private String jsonResult;
 
     /**
      * 操作状态（0正常 1异常）
@@ -113,140 +104,12 @@ public class OperateLog {
      * 操作时间
      */
     private Date operTime;
-
-    // public Long getOperId() {
-    //     return operId;
-    // }
-    //
-    // public void setOperId(Long operId) {
-    //     this.operId = operId;
-    // }
-    //
-    // public String getTitle() {
-    //     return title;
-    // }
-    //
-    // public void setTitle(String title) {
-    //     this.title = title;
-    // }
-    //
-    // public Integer getBusinessType() {
-    //     return businessType;
-    // }
-    //
-    // public void setBusinessType(Integer businessType) {
-    //     this.businessType = businessType;
-    // }
-    //
-    // public Integer[] getBusinessTypes() {
-    //     return businessTypes;
-    // }
-    //
-    // public void setBusinessTypes(Integer[] businessTypes) {
-    //     this.businessTypes = businessTypes;
-    // }
-    //
-    // public String getMethod() {
-    //     return method;
-    // }
-    //
-    // public void setMethod(String method) {
-    //     this.method = method;
-    // }
-    //
-    // public String getRequestMethod() {
-    //     return requestMethod;
-    // }
-    //
-    // public void setRequestMethod(String requestMethod) {
-    //     this.requestMethod = requestMethod;
-    // }
-    //
-    // public Integer getOperatorType() {
-    //     return operatorType;
-    // }
-    //
-    // public void setOperatorType(Integer operatorType) {
-    //     this.operatorType = operatorType;
-    // }
-    //
-    // public String getOperName() {
-    //     return operName;
-    // }
-    //
-    // public void setOperName(String operName) {
-    //     this.operName = operName;
-    // }
-    //
-    // public String getDeptName() {
-    //     return deptName;
-    // }
-    //
-    // public void setDeptName(String deptName) {
-    //     this.deptName = deptName;
-    // }
-    //
-    // public String getOperUrl() {
-    //     return operUrl;
-    // }
-    //
-    // public void setOperUrl(String operUrl) {
-    //     this.operUrl = operUrl;
-    // }
-    //
-    // public String getOperIp() {
-    //     return operIp;
-    // }
-    //
-    // public void setOperIp(String operIp) {
-    //     this.operIp = operIp;
-    // }
-    //
-    // public String getOperLocation() {
-    //     return operLocation;
-    // }
-    //
-    // public void setOperLocation(String operLocation) {
-    //     this.operLocation = operLocation;
-    // }
-    //
-    // public String getOperParam() {
-    //     return operParam;
-    // }
-    //
-    // public void setOperParam(String operParam) {
-    //     this.operParam = operParam;
-    // }
-    //
-    // public String getJsonResult() {
-    //     return jsonResult;
-    // }
-    //
-    // public void setJsonResult(String jsonResult) {
-    //     this.jsonResult = jsonResult;
-    // }
-    //
-    // public Integer getStatus() {
-    //     return status;
-    // }
-    //
-    // public void setStatus(Integer status) {
-    //     this.status = status;
-    // }
-    //
-    // public String getErrorMsg() {
-    //     return errorMsg;
-    // }
-    //
-    // public void setErrorMsg(String errorMsg) {
-    //     this.errorMsg = errorMsg;
-    // }
-    //
-    // public Date getOperTime() {
-    //     return operTime;
-    // }
-    //
-    // public void setOperTime(Date operTime) {
-    //     this.operTime = operTime;
-    // }
+    /**
+     * 请求参数
+     */
+    private String operParam;
+    /**
+     * 返回响应体
+     */
+    private String jsonResult;
 }

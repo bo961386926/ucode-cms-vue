22f6a40c6ae907b961d5d51eaf6158530e4dcb0c
explore <982361244@qq.com>
2022-03-22 21:08:50 +0800

    抽离出权限模块 ucode-cms-security

--- sql/cms（必选）.sql
+++ sql/cms（必选）.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 本地
- Source Server Type    : MySQL
- Source Server Version : 50735
- Source Host           : localhost:3306
- Source Schema         : ucode-cms
-
- Target Server Type    : MySQL
- Target Server Version : 50735
- File Encoding         : 65001
-
- Date: 18/01/2022 10:20:58
-*/
-
 SET NAMES utf8mb4;
 SET
 FOREIGN_KEY_CHECKS = 0;
